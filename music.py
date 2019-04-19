38d447c93315d4d01fe299c4a8e297af83f363ff
Ahmed Bafadal <a.m.bafadal@gmail.com>
2019-04-19 14:35:42 +0100

    First commit.

--- music.py
+++ music.py
@@ -2,6 +2,13 @@ from tkinter import *
 
 window = Tk()
 window.geometry('300x300')
-window.title("Ahmeds Python Music Player")
+window.title("Ahmed's Python Music Player")
+textLabel = Label(window, text="Play Button")
+textLabel.pack()
+
+photo = PhotoImage(file='round-play-button.png')
+playButton = Button(window, image=photo, command=printsomething)
+playButton.pack()
+
 
 window.mainloop()
