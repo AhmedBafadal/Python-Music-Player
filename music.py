b0068483fbf714a3b9d2e966d477274bb4c6c640
Ahmed Bafadal <a.m.bafadal@gmail.com>
2019-04-19 15:30:36 +0100

    Added play, stop and volume functionality.

--- music.py
+++ music.py
@@ -1,14 +1,38 @@
 from tkinter import *
+from pygame import mixer
 
 window = Tk()
+mixer.init()
 window.geometry('300x300')
 window.title("Ahmed's Python Music Player")
 textLabel = Label(window, text="Play Button")
 textLabel.pack()
 
+def play_music():
+    mixer.music.load("Fellin Myself.mp3")
+    mixer.music.play()
+
+def stop_music():
+    mixer.music.stop()
+
+def set_volume(value):
+    volume = int(value)/100
+    mixer.music.set_volume(volume)
+
+
+
 photo = PhotoImage(file='round-play-button.png')
-playButton = Button(window, image=photo, command=printsomething)
+playButton = Button(window, image=photo, command=play_music)
 playButton.pack()
+stopPhoto = PhotoImage(file='stop-button.png')
+stopButton = Button(window, image=stopPhoto, command=stop_music)
+stopButton.pack()
+
+scale = Scale(window, from_=0, to=100, orient=HORIZONTAL, command=set_volume)
+scale.set(70)
+scale.pack()
+
+
 
 
 window.mainloop()
